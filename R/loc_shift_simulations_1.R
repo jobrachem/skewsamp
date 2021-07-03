8b89c70bd47becc52e9e126152b4823d3d05f33e
dominik.strache <dominik.strache@stud.uni-goettingen.de>
2021-07-03 19:20:29 +0200

    Simulations 1

--- R/loc_shift_simulations_1.R
+++ R/loc_shift_simulations_1.R
@@ -1,15 +1,21 @@
 
-# Sample size estimator matrix
-# 200 Simulations for each case (8 cases in total)
-res_mat <- matrix(0, nrow = 200, ncol = 8)
+library(skewsamp)
+
+set.seed(1)
+
+
+nsim <- 10000
+res_mat <- matrix(0, nrow = nsim, ncol = 8)
 res <- matrix(0, nrow = 5, ncol = 8)
 rownames(res) <- c("Mean", "Variance", "q_10", "q_50", "q_90")
 
 
 
+
+
 # Case 1 (Normal distribution, m = 10, delta = 0.5)
 
-for(i in 1:200){
+for(i in 1:nsim){
   res_mat[i,1] <- n_locshift(s1 = rnorm(10), s2 = rnorm(10), alpha = 0.05, power = 0.9, delta = 0.5)
 }
 
@@ -23,7 +29,7 @@ res[5,1] <- quantile(res_mat[,1],0.9)
 
 # Case 2 (Normal distribution, m = 20, delta = 0.5)
 
-for(i in 1:200){
+for(i in 1:nsim){
   res_mat[i,2] <- n_locshift(s1 = rnorm(20), s2 = rnorm(20), alpha = 0.05, power = 0.9, delta = 0.5)
 }
 
@@ -37,7 +43,7 @@ res[5,2] <- quantile(res_mat[,2],0.9)
 
 # Case 3 (Uniform distribution, m = 10, delta = 0.2)
 
-for(i in 1:200){
+for(i in 1:nsim){
   res_mat[i,3] <- n_locshift(s1 = runif(10), s2 = runif(10), alpha = 0.05, power = 0.9, delta = 0.2)
 }
 
@@ -51,7 +57,7 @@ res[5,3] <- quantile(res_mat[,3],0.9)
 
 # Case 4 (Uniform distribution, m = 20, delta = 0.2)
 
-for(i in 1:200){
+for(i in 1:nsim){
   res_mat[i,4] <- n_locshift(s1 = runif(20), s2 = runif(20), alpha = 0.05, power = 0.9, delta = 0.2)
 }
 
@@ -65,7 +71,7 @@ res[5,4] <- quantile(res_mat[,4],0.9)
 
 # Case 5 (Exponential distribution, m = 10, delta = 0.35)
 
-for(i in 1:200){
+for(i in 1:nsim){
   res_mat[i,5] <- n_locshift(s1 = rexp(10), s2 = rexp(10), alpha = 0.05, power = 0.9, delta = 0.35)
 }
 
@@ -79,7 +85,7 @@ res[5,5] <- quantile(res_mat[,5],0.9)
 
 # Case 6 (Exponential distribution, m = 20, delta = 0.35)
 
-for(i in 1:200){
+for(i in 1:nsim){
   res_mat[i,6] <- n_locshift(s1 = rexp(20), s2 = rexp(20), alpha = 0.05, power = 0.9, delta = 0.35)
 }
 
@@ -93,7 +99,7 @@ res[5,6] <- quantile(res_mat[,6],0.9)
 
 # Case 7 (Logistic distribution, m = 10, delta = 0.8)
 
-for(i in 1:200){
+for(i in 1:nsim){
   res_mat[i,7] <- n_locshift(s1 = rlogis(10), s2 = rlogis(10), alpha = 0.05, power = 0.9, delta = 0.8)
 }
 
@@ -106,7 +112,7 @@ res[5,7] <- quantile(res_mat[,7],0.9)
 
 # Case 8 (Logistic distribution, m = 20, delta = 0.8)
 
-for(i in 1:200){
+for(i in 1:nsim){
   res_mat[i,8] <- n_locshift(s1 = rlogis(20), s2 = rlogis(20), alpha = 0.05, power = 0.9, delta = 0.8)
 }
 
@@ -116,4 +122,6 @@ res[2,8] <- var(res_mat[,8])
 res[3,8] <- quantile(res_mat[,8],0.1)
 res[4,8] <- quantile(res_mat[,8],0.5)
 res[5,8] <- quantile(res_mat[,8],0.9)
+
+write.csv(res, "loc_shift_simulations_1_results.csv")
 
