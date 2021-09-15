c4e03214b483cad3ef9bbd7bb40122eace4ad826
Johannes Brachem <jobrachem@posteo.de>
2021-09-15 12:58:42 +0200

    Name group ns n0 and n1

--- R/sample_size.R
+++ R/sample_size.R
@@ -4,8 +4,8 @@ new_sample_size <- function(n, two_sided, alpha, power, effect, effect_type, com
 
   structure(
     list(
-      n1 = n * q,
-      n2 = n * (1 - q),
+      n0 = n * q,
+      n1 = n * (1 - q),
       n = n,
       two_sided = two_sided,
       alpha = alpha,
@@ -41,11 +41,12 @@ sample_size <- function(n, two_sided, alpha, power, effect, effect_type,
 
 #' @export
 print.sample_size <- function(x, ...) {
-  cat("Estimated sample size for group difference.\n", x$comment, "\n\n")
+  cat("Estimated sample size for group difference.\n")
+  cat(x$comment, "\n\n")
 
-  cat("N (total)\t\t", x$n, "\n")
-  cat("N (Group 1)\t\t", x$n1, "\n")
-  cat("N (Group 2)\t\t", x$n2, "\n\n")
+  cat("N (total)\t\t", round(x$n, 2), "\n")
+  cat("n0 (Group 0)\t\t", round(x$n0, 2), "\n")
+  cat("n1 (Group 1)\t\t", round(x$n1, 2), "\n\n")
   cat("Effect size\t\t", x$effect, "\n")
   cat("Effect type\t\t", x$effect_type, "\n")
   cat("Type I error\t\t", x$alpha, "\n")
